a9854b64322d81b8b48222f9218d6dd6388f17a7
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2024-05-10 21:20:47 +0200

    Remove settings file and JSON handler

--- Utlis/json_handler.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-def load_instances():
-    INSTANCES_PATH = Path('./Data/instances.json')
-    with open(INSTANCES_PATH, 'r') as f:
-        data = json.load(f)
-
-    return data
